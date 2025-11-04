d41f3e1a4dba33972af13185712c8784974e4627
Subhajit Sen <sensubha91@gmail.com>
2025-11-04 11:24:46 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Badli Si Hawa Hai",
+    artist: "Kumaar",
+    img: "music-1",
+    src: "Badli-Si-Hawa-Hai-Mp3-Song-by-Kumaar(PagalWorldi.com.co)"
+  },
+  {
+    name: "Barbaad",
+    artist: "The Rish",
+    img: "Barbaad_(song).webp.png",
+    src: "Barbaad-Mp3-Song-by-The-Rish(PagalWorldi.com.co)"
+  },
+  {
+    name: "Bheegi Saree",
+    artist: "Adnan Sami",
+    img: "music-2",
+    src: "Bheegi-Saree-Mp3-Song-by-Adnan-Sami(PagalWorldi.com.co)"
+  },
+  {
+    name: "Heer",
+    artist: "Haider Ali",
+    img: "Heer-Urdu-2025-20250816175256-500x500",
+    src: "Heer-Mp3-Song-by-Haider-Ali(PagalWorldi.com.co)"
+  },
+  {
+    name: "Janaab E Aali",
+    artist: "Pritam",
+    img: "janaab",
+    src: "Janaab-E-Aali-Mp3-Song-by-Pritam(PagalWorldi.com.co)"
+  },
+  {
+    name: "Monica",
+    artist: "Anirudh Ravichander",
+    img: "monica",
+    src: "Monica-Mp3-Song-by-Anirudh-Ravichander(PagalWorldi.com.co)"
+  },
+  {
+    name: "Panwadi",
+    artist: "Khesari Lal Yadav",
+    img: "panwadi",
+    src: "Panwadi-Mp3-Song-by-Khesari-Lal-Yadav(PagalWorldi.com.co)"
+  }
+  // Add more songs by following the same format
+];
